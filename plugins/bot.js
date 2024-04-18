8ad907d44ce6f1994adcf7a2501a5d1923d10ab6
NEXA goat <144521464+henrygokugt99@users.noreply.github.com>
2024-04-18 12:21:40 +0300

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn}) => {
 let user = global.db.data.users[m.sender]
 let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let av = `./Assets/mp3/${pickRandom(["Xlicon", "Xlicon1", "Xlicon2", "Xlicon3", "Xlicon4"])}.mp3`
+let av = `./Assets/mp3/${pickRandom(["Nexa", "Nexa1", "Nexa2", "Nexa3", "Nexa4"])}.mp3`
 
-m.reply( `Hello ${taguser} Am Xlicon-V2 creatd by team Xlicon Need help?  type /help `)
+m.reply( `Hello ${taguser} Am Nexa-V1 creatd by team Nexa Need help?  type /help `)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
